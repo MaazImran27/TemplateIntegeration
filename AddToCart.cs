6efb5f38e51868efe2d137dbdf6b03cc8046bb1f
MaazImran27 <socialuse630@gmail.com>
2024-11-12 00:45:39 +0500

    Add files via upload

---
+++ AddToCart.cs
@@ -0,0 +1,13 @@
+﻿namespace Template_Integration.Models
+{
+	public class AddToCart
+	{
+		public int Id { get; set; }
+
+		public int ProductId { get; set; }
+
+		public Product Product { get; set; }
+
+		public int quantity { get; set; }
+	}
+}
